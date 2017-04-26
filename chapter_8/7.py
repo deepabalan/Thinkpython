9fe7562aa0687f56e561feba5fd1a148a7898cc5
deepabalan <deepa.deepabalan@gmail.com>
2017-04-26 18:48:34 +0530

    added problem 7

---
+++ chapter_8/7.py
@@ -0,0 +1,8 @@
+# There is a string method called count that is similar to the function
+# in the previous exercise. Read the docmunetation of this method and
+# write an invocation that counts the number of a's in 'banana'.
+
+
+string = 'banana'
+
+print string.count('a')
