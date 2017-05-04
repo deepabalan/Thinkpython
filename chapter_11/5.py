97fefa19f7f045017677d5a8ab8195b5d337f92b
deepabalan <deepa.deepabalan@gmail.com>
2017-05-04 08:19:50 +0530

    invert_dict using Built-in Type, setdefault.

--- chapter_11/5.py
+++ chapter_11/5.py
@@ -1,2 +1,27 @@
 # Read the documentation of the dictionary method set default and use
 # it to write a more concise version of invert_dict.
+
+
+def histogram(s):
+    t = {}
+    for c in s:
+        t[c] = t.get(c, 0) + 1
+    return t
+
+def invert_dict(d):
+    inverse = {}
+    for key in d:
+        value = d[key]
+        inverse.setdefault(value, []).append(key)
+    return inverse
+
+def proceed():
+    h = histogram(raw_input('Enter a string: '))
+    print invert_dict(h)
+    a = int(raw_input('Do you want to continue:(1/0): '))
+    if a == 1:
+        proceed()
+    elif a == 0:
+        return
+
+proceed()
